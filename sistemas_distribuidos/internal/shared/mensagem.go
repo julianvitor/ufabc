997cdeebab717f3272997e739de57214584a8ce0
Julian Vitor <julian.vtr@gmail.com>
2025-06-14 17:14:50 -0300

    struc da mensagem geral

--- sistemas_distribuidos/internal/shared/mensagem.go
+++ sistemas_distribuidos/internal/shared/mensagem.go
@@ -1,10 +1,9 @@
 package mensagem
 
 type Mensagem struct {
-    Tipo       string `json:"tipo"` // "PUT", "GET", "PUT_OK", etc.
-    Key        string `json:"key,omitempty"`
-    Value      string `json:"value,omitempty"`
-    Timestamp  int    `json:"timestamp,omitempty"`
-    ClienteIP  string `json:"cliente_ip,omitempty"`
-    ClientePorta string `json:"cliente_porta,omitempty"`
+	Operacao    string `json:"operacao"`
+	Chave       string `json:"chave"`
+	Valor       string `json:"valor,omitempty"`
+	Timestamp   int64  `json:"timestamp,omitempty"`
+	ClienteAddr string `json:"cliente_addr,omitempty"`
 }
--- sistemas_distribuidos/internal/shared/net.go
+++
@@ -1,2 +0,0 @@
-func EnviarMensagem(conn net.Conn, msg mensagem.Mensagem) error
-func ReceberMensagem(conn net.Conn) (mensagem.Mensagem, error)
